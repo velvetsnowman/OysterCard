67b73130d093af39f54f4ff7758b090eea3417a0
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-16 15:25:29 +0100

    challenge 4 finished, added an initial balance' exit exit
    '

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source "https://rubygems.org"
 
+ruby '2.5.1'
+
 group :development, :test do
   gem 'rspec'
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
+require 'oystercard'
 describe Oystercard do
+
+  describe '#balance' do
+    it { is_expected.to respond_to(:balance) }
+    it 'should return a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
 end
