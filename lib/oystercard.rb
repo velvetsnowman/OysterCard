4ed5159a92e07d335c06a976145caac035eb7645
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-18 06:24:36 +0100

    challenge 9 and refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
   attr_reader :balance
   MAXBALANCE = 90
+  MINBALANCE = 1
   def initialize
     @balance = 0
   end
 
   def topup(amount)
-    fail "The limit of" + "#{MAXBALANCE}" + "has been reached." if @balance + amount > MAXBALANCE
+    fail "The limit of #{MAXBALANCE} has been reached." if @balance + amount > MAXBALANCE
     @balance += amount
   end
 
@@ -15,14 +16,15 @@ class Oystercard
   end
 
   def in_journey?
-    false
+    @commuting
   end
 
-  # def touch_in
-  #   @commuting = true
-  # end
-  #
-  # def touch_out
-  #   @commuting = false
-  # end
+  def touch_in
+    fail 'Cannot travel: insufficient funds' if @balance < MINBALANCE
+    @commuting = true
+  end
+
+  def touch_out
+    @commuting = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,52 +1,63 @@
 require 'oystercard'
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
   describe '#balance' do
     it { is_expected.to respond_to(:balance) }
     it 'should return a balance of zero' do
-      expect(subject.balance).to eq(0)
+      expect(oystercard.balance).to eq(0)
     end
   end
 
   describe '#topup' do
     it { is_expected.to respond_to(:topup).with(1).argument }
     it 'should top up 10£' do
-      expect{(subject.topup(10))}.to change{ subject.balance }.to(10)
+      expect{(oystercard.topup(10))}.to change{ oystercard.balance }.to(10)
     end
     it 'raises and error if the maximum balance is exceeded' do
       max_balance = Oystercard::MAXBALANCE
-      subject.topup(max_balance)
-      expect {subject.topup(1)}.to raise_error ("The limit of" + "#{max_balance}" + "has been reached.")
+      oystercard.topup(max_balance)
+      expect {oystercard.topup(1)}.to raise_error ("The limit of #{max_balance} has been reached.")
     end
   end
 
   describe '#deduct' do
     it { is_expected.to respond_to(:deduct).with(1).argument }
     it 'should deduct 10£ from the balance' do
-      subject.topup(50)
-      expect{(subject.deduct(10))}.to change{subject.balance}.to(40)
+      oystercard.topup(50)
+      expect{(oystercard.deduct(10))}.to change{oystercard.balance}.to(40)
     end
   end
 
   describe '#in_journey?' do
     it { is_expected.to respond_to(:in_journey?)}
     it 'should state if the passenger is in a journey or not' do
-      expect(subject.in_journey?).to_not be true
+      expect(oystercard).not_to be_in_journey
     end
   end
 
-  # describe '#touch_in' do
-  #   it { is_expected.to respond_to(:touch_in)}
-  #   it 'should let a passenger touch in' do
-  #     expect(subject.touch_in).to be true
-  #   end
-  # end
-  #
-  # describe '#touch_out' do
-  #   it { is_expected.to respond_to(:touch_out)}
-  #   it 'should let a passenger touch out' do
-  #     expect(subject.touch_out).to be false
-  #   end
-  # end
+  describe '#touch_in' do
+    it { is_expected.to respond_to(:touch_in)}
+    it 'should let a passenger touch in' do
+      oystercard.topup(1.5)
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+    it 'should raise an error when not enough funds' do
+      oystercard.topup(0.5)
+      expect {oystercard.touch_in}.to raise_error('Cannot travel: insufficient funds')
+    end
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to(:touch_out)}
+    it 'should let a passenger touch out' do
+      oystercard.topup(1.5)
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
+  end
 
 end
